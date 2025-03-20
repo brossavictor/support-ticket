256a1ce61f961da2ba494ca06282c0e9619e899f
Victor Brossa <brossajoao@gmail.com>
2025-03-19 21:21:51 -0400

    updated ticket obj

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,11 +1,19 @@
-export function create({ request, response }) {
+import { randomUUID } from "node:crypto";
+
+export function create({ request, response, database }) {
   const { equipment, description, user_name } = request.body;
 
   const ticket = {
+    id: randomUUID(),
     equipment,
     description,
     user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
   };
 
-  return response.end(JSON.stringify(ticket));
+  database.insert("ticket", ticket);
+
+  return response.writeHead(201).end(JSON.stringify(ticket));
 }
